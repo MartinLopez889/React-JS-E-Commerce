84502c11fa6eaf2a1ab4c00c008132468d4aae92
Martin Lopez <martin.lopez.889@gmail.com>
2023-10-03 22:11:57 -0300

    PreEntrega2 Lopez

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,9 +3,9 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 
-function Item ({ id, title, description, price, pictureUrl, }){
+function Item({ id, title, description, price, pictureUrl, }) {
   return (
-    <Card style={{ width: '18rem', height:500 }}>
+    <Card style={{ width: '18rem', height: 500 }}>
       <Card.Img variant="top" src={pictureUrl} />
       <Card.Body>
         <Card.Title>{id} {title}</Card.Title>
